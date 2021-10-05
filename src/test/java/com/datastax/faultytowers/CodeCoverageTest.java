3e89158b3cf9f38bbae06c72b02d9dff1feddbc5
Matt Fleming <matt@codeblueprint.co.uk>
2021-10-05 23:02:14 +0100

    WIP

--- src/test/java/com/datastax/faultytowers/CodeCoverageTest.java
+++ src/test/java/com/datastax/faultytowers/CodeCoverageTest.java
@@ -1,16 +1,25 @@
 package com.datastax.faultytowers;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
+import static org.junit.Assert.assertNotEquals;
 
 public class CodeCoverageTest {
 
     private static final String OUTPUTFILE = "writeOutputFile.txt";
+
+    @Before
+    public void setUp() throws IOException {
+        // Delete contents of file
+        new FileOutputStream(OUTPUTFILE).close();
+    }
 
     @AfterClass
     public static void cleanup() {
@@ -22,7 +31,19 @@ public class CodeCoverageTest {
     public void noCoverageWritesEmptyFile() throws IOException {
         FaultyTowers ft = new FaultyTowers();
         ft.writeCoverage(OUTPUTFILE);
+        assertOutputFileExists();
+    }
+
+    private void assertOutputFileExists() {
         File f = new File(OUTPUTFILE);
         assertTrue(f.exists());
+    }
+
+    @Test
+    public void coverageDataWritesNonEmptyFile() throws IOException {
+        FaultyTowers ft = new FaultyTowers();
+        ft.writeCoverage(OUTPUTFILE);
+        assertOutputFileExists();
+        assertNotEquals(0, new File(OUTPUTFILE).length());
     }
 }
